9b28090e7003ec55a2c23053e80c2bedf791dc67
Zeb Matteson <zmatteson@gmail.com>
2018-02-13 18:04:12 -0600

    added count_and_say

--- solutions/count_and_say.py
+++ solutions/count_and_say.py
@@ -1,2 +1,34 @@
+"""
+The count-and-say sequence is the sequence of integers with the first five terms as following:
+
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+Given an integer n, generate the nth term of the count-and-say sequence.
+
+Note: Each term of the sequence of integers will be represented as a string.
+"""
+
 def get_sequence(i):
-    return 0
+    ans = '1'
+    for x in range(1,i):
+        count = 0
+        l, r = 0, 0
+        while (r<=len(ans)):
+            if r == len(ans):
+                ans = ans[:l] + str(count) + ans[l]
+                break
+            elif ans[l]==ans[r]:
+                count += 1
+                r +=1
+            else:
+                ans = ans[:l] + str(count) + ans[l] + ans[r:]
+                r += 2 - count
+                count = 0
+                l = r
+    return int(ans) 
--- solutions/tests/test_count_and_say.py
+++ solutions/tests/test_count_and_say.py
@@ -1,6 +1,25 @@
 import pytest
 from solutions.count_and_say import *
 # input of one should return "one"
-def test_one_returns_one_one():
+def test_one_returns_one():
     x = get_sequence(1)
     assert x == 1
+
+def test_two_returns_one_one():
+    x = get_sequence(2)
+    assert x == 11
+
+def test_three_returns_21():
+    x = get_sequence(3)
+    assert x == 21
+
+def test_four_returns_1211():
+    x = get_sequence(4)
+    assert x == 1211
+
+def test_five_returns_111221():
+    x = get_sequence(5)
+    assert x == 111221
+def test_six_returns_312211():
+    x = get_sequence(6)
+    assert x == 312211
