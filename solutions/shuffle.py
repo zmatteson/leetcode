c59e36e5f8a4c2bc0ac6cf9c03e50d60ac7b4cb9
Zeb Matteson <zmatteson@gmail.com>
2018-03-27 14:59:07 -0500

    adding solutions

---
+++ solutions/shuffle.py
@@ -0,0 +1,56 @@
+"""
+Shuffle a set of numbers without duplicates.
+
+Example:
+
+// Init an array with set 1, 2, and 3.
+int[] nums = {1,2,3};
+Solution solution = new Solution(nums);
+
+// Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3] must equally likely to be returned.
+solution.shuffle();
+
+// Resets the array back to its original configuration [1,2,3].
+solution.reset();
+
+// Returns the random shuffling of array [1,2,3].
+solution.shuffle();
+"""
+
+import random
+
+class Solution:
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.nums = nums
+
+    def reset(self):
+        """
+        Resets the array to its original configuration and return it.
+        :rtype: List[int]
+        """
+        return self.nums
+
+    def shuffle(self):
+        """
+        Returns a random shuffling of the array.
+        :rtype: List[int]
+        """
+        temp = list(self.nums)
+        random.shuffle(temp)
+        return temp
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
+
+if __name__ == '__main__':
+    mylist = Solution([1,2,3])
+    print(mylist.reset())
+    print(mylist.shuffle())
+    print(mylist.reset())
